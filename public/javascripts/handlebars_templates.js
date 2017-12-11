3e860f3c3bf0f81de790db75dea5279229dae846
Tovi Newman <deartovi@gmail.com>
2017-12-11 00:00:34 -0800

    drag and drop list updates collection order

--- public/javascripts/handlebars_templates.js
+++ public/javascripts/handlebars_templates.js
@@ -21,11 +21,11 @@ this["JST"]["card"] = Handlebars.template({"1":function(container,depth0,helpers
 this["JST"]["list"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"board_item\">	<div class=\"list\" data-id=\""
+  return "<div class=\"list\" data-id=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">		<form class=\"renameList\">			<input type=\"text\">			<input type=\"submit\">		</form>		<h1>"
+    + "\">	<form class=\"renameList\">		<input type=\"text\">		<input type=\"submit\">	</form>	<h1>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h1>		<div class=\"cards\">		</div>		<div class=\"new_card_form\">			<form>				<textarea></textarea>				<input type=\"submit\" value=\"Add\" class=\"save\">				<div class=\"cancel\">X</div>			</form>		</div>		<div class=\"card_form_toggle\">Add a card...</div>	</div></div>";
+    + "</h1>	<div class=\"cards\">	</div>	<div class=\"new_card_form\">		<form>			<textarea></textarea>			<input type=\"submit\" value=\"Add\" class=\"save\">			<div class=\"cancel\">X</div>		</form>	</div>	<div class=\"card_form_toggle\">Add a card...</div></div>";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
--- public/javascripts/views/listsView.js
+++ public/javascripts/views/listsView.js
@@ -5,14 +5,15 @@ var ListsView = Backbone.View.extend({
   events: {
     'click .card_form_toggle': 'newCardFormToggle',
     'click .cancel': 'newCardFormToggle',
+    'keypress textarea': 'handleTextArea',
     'submit .new_card_form form': 'addCard',
     'click .list h1': 'showListNameForm',
-    'submit .renameList': 'renameList'
+    'submit .renameList': 'renameList',
   },
   renameList: function(e) {
     e.preventDefault();
     $form = $(e.target);
-    var id = this.getID(e);
+    var id = this.getListID(e);
     var title = $form.find('input[type="text"]').val();
     if ($form.siblings('h1').html() === title) this.toggleListNameForm(e);
     else this.collection.get(id).set({title: title});
@@ -34,27 +35,58 @@ var ListsView = Backbone.View.extend({
     $input.select();
   },
   parentList: function(e) {
-    return $(e.target).closest('.list')
+    if (e.target) return $(e.target).closest('.list');
+    return $(e).closest('.list');
   },
-  getID: function(e) {
+  getListID: function(e) {
     return this.parentList(e).attr('data-id');
+  },
+  handleTextArea: function(e) {
+    if (e.key === 'Enter') this.addCard(e);
   },
   addCard: function(e) {
     e.preventDefault();
-    var title = $(e.target).find('textarea').val();
-    var list = this.collection.get(this.getID(e));
+    var title = $(e.target).closest('.list').find('textarea').val();
+    var list = this.collection.get(this.getListID(e));
     list.cards.add({title: title, id: list.serialID++})
     this.collection.trigger('change');
   },
   newCardFormToggle: function(e) {
     var $formToggle = this.parentList(e).find('.card_form_toggle');
     var $cardForm = $formToggle.siblings('.new_card_form');
+    var $input = $cardForm.find('textarea');
     $cardForm.slideToggle();
     $formToggle.slideToggle();
+    $input.focus();
   },
   render: function() {
     this.$el.html(this.listsTemplate({ lists: this.collection.toJSON() }));
     this.collection.forEach(list => this.renderCards(list));
+    this.setDrags();
+  },
+  setDrags: function() {
+    var isCard = {
+      invalid: function (target) {
+        return !!$(target).closest('.card').length;
+      }
+    }
+    this.listDrags = dragula([this.el, this.el], isCard);
+    this.cardDrags = dragula($('.cards').toArray());
+    this.setDragListeners();
+  },
+  setDragListeners: function() {
+    this.listDrags.on('drop', function(el, _, _, sibling) {
+      var index = -1;
+      var model = this.collection.get(this.getListID(el));
+      this.collection.remove(model, {silent: true});
+      if (sibling !== null) {
+        var siblingID = this.getListID(sibling);
+        var sibling = this.collection.get(siblingID)
+        index = +this.collection.indexOf(sibling);
+      }
+
+      this.collection.add(model, {at: index, silent: true});
+    }.bind(this));
   },
   renderCards: function(list) {
     var id = list.get('id');
